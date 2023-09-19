a9d07f3b84cb8fbc511ccf61eaa554f18ac6dd1b
taehwee_park <hweetae@gmail.com>
2023-09-19 21:20:56 +0900

    [Typo] correcting typos in sql statements
    description:
    - insert int -> insert into

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INT member (id, name) VALUES (1, 'Hong')
-INSERT INT member (id, name) VALUES (2, 'Gil')
-INSERT INT member (id, name) VALUES (3, 'Dong')
+INSERT INTO member (id, name) VALUES (1, 'Hong')
+INSERT INTO member (id, name) VALUES (2, 'Gil')
+INSERT INTO member (id, name) VALUES (3, 'Dong')
