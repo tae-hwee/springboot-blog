d23dd71fc3d3e1b93c8c3df6541c1fa2bb8f19a1
taehwee_park <hweetae@gmail.com>
2023-09-22 17:39:13 +0900

    [New Feature] article (all) get service

---
+++ blog-practice/src/main/java/me/taehwee/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package me.taehwee.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.taehwee.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
--- blog-practice/src/main/java/me/taehwee/springbootdeveloper/service/BlogService.java
+++ blog-practice/src/main/java/me/taehwee/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.taehwee.springbootdeveloper.dto.AddArticleRequest;
 import me.taehwee.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -13,5 +15,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
