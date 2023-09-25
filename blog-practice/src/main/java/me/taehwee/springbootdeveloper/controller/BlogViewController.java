f4af8edb2a4c14ae8fd68ba9d8a4618e431cfbab
taehwee_park <hweetae@gmail.com>
2023-09-25 16:45:51 +0900

    [New Feature] displaying aritcle list

---
+++ blog-practice/src/main/java/me/taehwee/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,28 @@
+package me.taehwee.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.taehwee.springbootdeveloper.dto.ArticleListViewResponse;
+import me.taehwee.springbootdeveloper.dto.ArticleResponse;
+import me.taehwee.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
