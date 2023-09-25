b7a26fcc4b51afb63035d849ff3e299ca7a998f7
taehwee_park <hweetae@gmail.com>
2023-09-25 15:54:16 +0900

    [New Feature] thymeleaf practice

---
+++ blog-practice/src/main/java/me/taehwee/springbootdeveloper/controller/ExampleController.java
@@ -0,0 +1,36 @@
+package me.taehwee.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person examplePerson = new Person();
+        examplePerson.setId(1L);
+        examplePerson.setName("HongGilDong");
+        examplePerson.setAge(20);
+        examplePerson.setHobbies(List.of("working out", "reading books"));
+
+        model.addAttribute("person", examplePerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example";
+    }
+
+    @Setter
+    @Getter
+    class Person {
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
