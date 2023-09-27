b362df240e6f2881ecb3c2c2487d2fa057929c5f
taehwee_park <hweetae@gmail.com>
2023-09-27 17:47:07 +0900

    [New Feature] Review

---
+++ blog-review/blog-review/src/main/java/me/taehwee/blogreview/BlogReviewApplication.java
@@ -0,0 +1,11 @@
+package me.taehwee.blogreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogReviewApplication.class, args);
+    }
+}
---
+++ blog-review/blog-review/src/main/java/me/taehwee/blogreview/TestController.java
@@ -0,0 +1,13 @@
+package me.taehwee.blogreview;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
